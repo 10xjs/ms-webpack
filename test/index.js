c8eef7b3d5624c520d3638810b1209af1920e4e3
Neal Granger <neal@nealg.com>
2015-09-19 00:54:08 -0700

    Tests

---
+++ test/index.js
@@ -0,0 +1,85 @@
+/* eslint-disable */
+var Metalsmith = require('metalsmith');
+var assert = require('assert');
+var webpack = require('..');
+var rm = require('rimraf').sync;
+var path = require('path');
+var fs = require('fs');
+var assertDirEqual = require('assert-dir-equal')
+
+console.log(path.resolve(__dirname, 'fixture/src'));
+
+var config = {
+  context: 'test/fixture/src',
+  entry: './js/index.js',
+  output: {
+    path: 'test/build',
+    publicPath: "/",
+    filename: 'js/[name].js'
+  },
+  devtool: 'source-map'
+}
+
+var metadata = {
+  assets: {
+    'main.js': '/js/main.js',
+    'main.map': '/js/main.js.map'
+  },
+  assetsByType: {
+    js: [ '/js/main.js' ],
+    map: [ '/js/main.js.map' ]
+  }
+}
+
+describe('metalsmith-webpack', function() {
+  afterEach(function() {
+    rm('test/build');
+  });
+
+  it('should resolve paths', function() {
+    var resolvePaths = require('../dist/').MetalsmithWebpack.resolvePaths;
+    var resolved = resolvePaths(config);
+
+    assert.equal(path.resolve(config.context), resolved.context);
+    assert.equal(path.resolve(config.output.path), resolved.output.path);
+  });
+
+  it('should output files', function(done) {
+    var m = Metalsmith(__dirname)
+      .source('fixture/src')
+      .destination('build')
+      .ignore('js')
+      .use(webpack(config))
+      .build(function(err, files){
+        if (err) return done(err);
+        assertDirEqual('test/fixture/build', 'test/build');
+        done();
+      });
+  });
+
+  it('should set metadata', function(done) {
+    var m = Metalsmith(__dirname)
+      .source('fixture/src')
+      .destination('build')
+      .ignore('js')
+      .use(webpack(config));
+
+    m.build(function(err, files){
+      if (err) return done(err);
+      assert.deepEqual(m.metadata().webpack, metadata);
+      done();
+    });
+  });
+
+  it('should throw error if output path is outside of metalsmith destination', function(done) {
+    var m = Metalsmith(__dirname)
+      .source('fixture/src')
+      .destination('foo')
+      .ignore('js')
+      .use(webpack(config))
+      .build(function(err, files) {
+        assert.equal(err.name, 'DestinationError');
+        done();
+      });
+  });
+});
